4702aae57b5880ddcb764d55d13c95ddafbb45f8
T0M1B3T4 <141879158+T0M1B3T4@users.noreply.github.com>
2024-08-19 22:47:52 -0500

    Primer Commit
    Agregué el proyecto al repositorio

---
+++ Gestión Museo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using static System.Console;
+using static System.Convert;
+
+namespace Gestión_Museo
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
